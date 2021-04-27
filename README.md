3d57616b2beea55a3420e891217ac31d85759dd7
rick kauffman <rick@rickkauffman.com>
2021-04-27 16:21:23 -0700

    initial release

---
+++ README.md
@@ -0,0 +1,72 @@
+# HPE OneView Integration Pack
+This pack allows you to integrate with HPE OneView
+
+## Configuration
+Copy the example configuration in [hpeoneview.yaml.example](./hpeoneview.yaml.example) to
+`/opt/stackstorm/configs/hpeoneview.yaml` and edit as required.
+
+It must contain:
+
+```
+ipaddress - Your OneView appliance IP address
+username - OneView User name
+password - OneView Password
+dbuser  - Mongo database username
+dbpass - Mongo database password
+```
+
+You can also use dynamic values from the datastore. See the
+[docs](https://docs.stackstorm.com/reference/pack_configs.html) for more info.
+
+Example configuration:
+
+```yaml
+---
+  ipaddress: "10.10.10.10"
+  username: "Administrator"
+  password: "password"
+  dbuser: "appUser"
+  dbpass: "passwordForAppUser"
+```
+You can also run `st2 pack config hpeoneview` and answer the prompts
+
+**Note** : When modifying the configuration in `/opt/stackstorm/configs/` please
+           remember to tell StackStorm to load these new values by running
+           `st2ctl reload --register-configs`
+
+
+## Actions
+
+Actions are defined in two groups:
+
+### Individual actions: GET, POST, PUT with under bar will precede each individual action
+* ``get_interconnects``
+* ``get_network_sets``
+* ``get_`enclosure``
+* ``post_fabrics``
+
+### Orquestra Workflows: will not
+
+This application uses the mongo db installed by StackStorm. Since the DB is secured
+you will need to log into the StackStorm mongo DB as a StackStorm admin and create a separate DB
+
+# To get this pack to work with StackStorm mongo DB
+
+log in with admin first
+--------------------------------------------------------------------------------------
+mongo -u admin -p UkIbDILcNbMhkh3KtN6xfr9h admin  (passwd in /etc/st2/st2.config)
+
+# Then create a new user
+db.createUser({user: "appUser",pwd: "passwordForAppUser",roles: [ { role: "readWrite", db: "app_db" } ]})
+
+# Then if necessary you can check the mongo database records by
+mongo -u appUser -p passwordForAppUser admin
+
+# Servicenow Tables
+![Server Hardware - server hardware information](/img/server.png)
+Server fields in service now: u_vendor,u_created,u_assettag,u_etag,u_hostostype,u_memorymb,u_model,u_firmware,
+u_hostname,u_address,u_mpmodel,u_mpstate,u_status,u_uuid,u_serno
+
+![OneView Alarms - Alarm information](/img/alarm.png)
+Alarms fields in service now:
+u_vendor,u_sev,u_desc,u_uuid,u_created
---
+++ actions/load-hpeov-server-hardware.py
@@ -0,0 +1,58 @@
+# (C) Copyright 2019 Hewlett Packard Enterprise Development LP.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law. or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# __author__ = "@netwookie"
+# __credits__ = ["Rick Kauffman"]
+# __license__ = "Apache2.0"
+# __maintainer__ = "Rick Kauffman"
+# __email__ = "rick.a.kauffman@hpe.com"
+
+# A python script for getting a dictionary of switches
+
+import pymongo
+from lib.actions import MongoBaseAction
+
+
+class loadDb(MongoBaseAction):
+    def run(self, servers):
+
+        mydb = self.dbclient["app_db"]
+        known = mydb["ovservers"]
+
+        new_server={}
+
+        for server in servers:
+            myquery = { "_id" : server['created'] }
+            records = known.find(myquery).count()
+            if records == 0:
+                new_server['u_vendor']='hpe-oneview'
+                new_server['_id']=server['created']
+                new_server['u_created']=server['created']
+                new_server['u_assettag']=server['assetTag']
+                new_server['u_etag']=server['eTag']
+                new_server['u_hostostype']=server['hostOsType']
+                new_server['u_memorymb']=server['memoryMb']
+                new_server['u_model']=server['model']
+                new_server['u_firmware']=server['mpFirmwareVersion']
+                new_server['u_hostname']=server['mpHostInfo']['mpHostName']
+                new_server['u_address']=server['mpHostInfo']['mpIpAddresses'][0]['address']
+                new_server['u_mpmodel']=server['mpModel']
+                new_server['u_mpstate']=server['mpState']
+                new_server['u_status']=server['status']
+                new_server['u_uuid']=server['uuid']
+                new_server['u_serno']=server['virtualSerialNumber']
+                new_server['u_process']='no'
+                write_record = known.insert_one(new_server)
+                # write_record = process.insert_one(alarm)
+        return (records)
